5643b526371662ff48fc1e8bfdba3ccf8449535f
Oriooonnn <jasko.kusnierz@vp.pl>
2025-10-29 22:34:20 +0100

    New comments and translation

--- script.py
+++ script.py
@@ -1,107 +1,119 @@
-from cProfile import label
+"""
+Heart Disease Data Analysis
+===========================
+Dataset: Kaggle Heart Disease Dataset
+Goal: Exploratory Data Analysis to identify risk factors
+Author: Jan Kuśnierz
+Date:29.10.2025
 
+Analysis Includes:
+-Data Cleaning
+-Visualization (Histograms, Barcharts, Boxplot)
+-Key findings (Age,Gender,Cholesterol)
+"""
+
+
+
+#=========IMPORTS==============
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from IPython.core.pylabtools import figsize
-from pandas.plotting import boxplot
-#Otwarcie pliku csv
+
+
 hearts = pd.read_csv('heart.csv')
-#Wydrukowanie Pierwszych Pięciu
-pierwsze = hearts.head()
-ksztalt = hearts.shape
-informacje = hearts.info
-opisz = hearts.describe()
-dane = hearts['HeartDisease'].value_counts()
-kategorie = dane.index
-liczby = dane.values
-#Deklaracja nowego wykresu
+first = hearts.head()
+shape = hearts.shape
+information = hearts.info
+description = hearts.describe()
+data = hearts['HeartDisease'].value_counts()
+category = data.index
+numbers = data.values
 plt.figure(figsize=(10,10))
-#Wykres
-plt.bar(kategorie, liczby)
-#Podziałki na wykresie
-plt.xticks([0,1],["Zdrowi","Chorzy"])
+plt.bar(category, numbers)
+plt.xticks([0,1],["Heart disease","No Heart disease"])
 plt.xlabel("Status")
-plt.ylabel("Ilość Badanych")
-plt.title("Rozkład pacjentów z choroba serca")
+plt.ylabel("Amount of patients")
+plt.title("Distribution of patients with heart disease")
 plt.show()
 plt.figure(figsize=(10,10))
-#Deklaracja Histogramu
 plt.hist(hearts['Age'], bins=10)
-plt.xlabel("Wiek")
-plt.ylabel("Ilość Badanych")
+plt.xlabel("Age")
+plt.ylabel("Amount of patients")
 plt.show()
-print(opisz)
-print(informacje)
-print(pierwsze)
-print(ksztalt)
+print(description)
+print(information)
+print(first)
+print(shape)
 print(hearts.shape)
 print(hearts.info())
+#Cleaning Data from Hearts.csv(Abstract cholesterol numbers)
 hearts_clean = hearts[(hearts["Cholesterol"] > 0) & (hearts["Cholesterol"] < 400)]
 print(hearts.describe())
 (hearts['HeartDisease'].value_counts())
-Chore_Kobiety = len(hearts[(hearts['Sex'] == "F") & (hearts['HeartDisease'] ==1) & (hearts['Age'] > 50)])
-Chorzy_Mezczyzni = len(hearts[(hearts['Sex'] == "M") & (hearts["HeartDisease"] ==1) & (hearts['Age'] > 50)])
-Dane = [Chore_Kobiety, Chorzy_Mezczyzni]
-Plec = ["Kobieta", "Mezczyzna"]
+Disease_Women = len(hearts[(hearts['Sex'] == "F") & (hearts['HeartDisease'] ==1) & (hearts['Age'] > 50)])
+Disease_Men = len(hearts[(hearts['Sex'] == "M") & (hearts["HeartDisease"] ==1) & (hearts['Age'] > 50)])
+Sex_Data = [Disease_Women, Disease_Men]
+Sex = ["Woman", "Man"]
 plt.figure(figsize=(8,6))
-plt.bar(Plec, Dane)
-plt.xlabel("Płeć")
-plt.ylabel("Ilość zachorowań")
+#Bar chart: Difference between men and women with heart diseases
+plt.bar(Sex, Sex_Data)
+plt.xlabel("Sex")
+plt.ylabel("Amount of diseases")
 plt.show()
 plt.figure(figsize=(8,6))
-Sredni_Cholestero = int(hearts['Cholesterol'].mean())
-print(Sredni_Cholestero)
-hearts.boxplot(column="HeartDisease",by="Cholesterol")
-plt.xlabel("Zachorowania")
-plt.ylabel("Cholesterol")
-plt.show()
-zdrowi = hearts_clean[hearts_clean["HeartDisease"] == 0]
-chorzy = hearts_clean[hearts_clean["HeartDisease"] == 1]
+Average_Cholesterol = int(hearts['Cholesterol'].mean())
+print(Average_Cholesterol)
+no_heart_disease = hearts_clean[hearts_clean["HeartDisease"] == 0]
+heart_disease = hearts_clean[hearts_clean["HeartDisease"] == 1]
 
 plt.figure(figsize=(10,6))
-plt.hist((zdrowi["Age"]),bins=15,alpha=0.6,color="green",label= "Zdrowi")
-plt.hist((chorzy["Age"]),bins=15,alpha=0.6,color="red",label="Chorzy")
-plt.xlabel("Wiek")
-plt.ylabel("Liczba Pacjentów")
+#Histogram: Age and heart diseases
+plt.hist((no_heart_disease["Age"]),bins=15,alpha=0.6,color="green",label= "Zdrowi")
+plt.hist((heart_disease["Age"]),bins=15,alpha=0.6,color="red",label="Chorzy")
+plt.xlabel("Age")
+plt.ylabel("Amount of patients")
 plt.legend()
 plt.show()
-po_60 = hearts[hearts["Age"] > 60]
-chorzy_po_60 = po_60[po_60["HeartDisease"] == 1]
-procent = (len(chorzy_po_60)/ len(po_60)) * 100
-print(f"Osób po 60 {len(po_60)}")
-print(f"W tym chorych {len(chorzy_po_60)}")
+#What percentage of people over 60 has a heart disease?
+after_60 = hearts[hearts["Age"] > 60]
+disease_after_60 = after_60[after_60["HeartDisease"] == 1]
+procent = (len(disease_after_60)/ len(after_60)) * 100
+print(f"Osób po 60 {len(after_60)}")
+print(f"W tym chorych {len(disease_after_60)}")
 print(f"Co stanowi {procent} procent")
-mezczyzni = hearts[hearts["Sex"] == "M"]
-mezczyzni_chorzy = mezczyzni[mezczyzni["HeartDisease"] == 1]
-kobiety = hearts[hearts["Sex"] == "F"]
-kobiety_chore = kobiety[kobiety["HeartDisease"] == 1]
-procent_m = (len(mezczyzni_chorzy) / len(mezczyzni)) * 100
-procent_f = (len(kobiety_chore) / len(kobiety)) * 100
-Plcie = ["Mezczyzni", "Kobiety"]
-Procenty = [procent_m, procent_f]
+Man = hearts[hearts["Sex"] == "M"]
+Man_Heart_Disease = Man[Man["HeartDisease"] == 1]
+Woman = hearts[hearts["Sex"] == "F"]
+Woman_Heart_Disease = Woman[Woman["HeartDisease"] == 1]
+percentage_m = (len(Man_Heart_Disease) / len(Man)) * 100
+percentage_f = (len(Woman_Heart_Disease) / len(Woman)) * 100
+Sex2 = ["Men", "Women"]
+Percentages = [percentage_m, percentage_f]
 plt.figure(figsize=(8,6))
-plt.bar(Plcie,Procenty,color=["blue","red"],edgecolor="black")
-plt.xlabel("Płeć")
-plt.ylabel("Procenty")
+#Comparision of Men and women in case of heart diseases with percentages
+plt.bar(Sex2,Percentages,color=["blue","red"],edgecolor="black")
+plt.xlabel("Sex")
+plt.ylabel("Percentage")
 plt.ylim(0,100)
 plt.grid(axis= "y", alpha = 0.3)
 plt.show()
 plt.figure(figsize=(8,6))
-plt.boxplot([zdrowi["Cholesterol"],chorzy["Cholesterol"]], labels=["Zdrowi","Chorzy"], patch_artist=True,boxprops=dict(facecolor="lightblue"),medianprops=dict(color="red",linewidth=2))
-plt.xlabel("Wiek")
+#Boxplot: Does cholesterol really matter in case of heart diseases?
+plt.boxplot([no_heart_disease["Cholesterol"],heart_disease["Cholesterol"]], labels=["Heart Disease","No Heart Disease"], patch_artist=True,boxprops=dict(facecolor="lightblue"),medianprops=dict(color="red",linewidth=2))
+plt.xlabel("Age")
 plt.ylabel("Cholesterol")
-plt.title("Zależność cholesterolu wśród zdrowych i chorych")
+plt.title("Cholesterol among people with heart disease and without heart disease")
 plt.grid(axis="y", alpha =0.3)
 plt.show()
 hearts["Cholesterol"] = hearts["Cholesterol"].replace(0,np.nan)
 
 
 
-print(f"Zdrowi {zdrowi["Cholesterol"].min()}")
-print(f"Zdrowi {zdrowi["Cholesterol"].max()}")
-print(f"Chorzy {chorzy["Cholesterol"].min()}")
-print(f"Chorzy {chorzy["Cholesterol"].max()}")
+print(f"Without Heart Disease {no_heart_disease["Cholesterol"].min()}")
+print(f"Without Heart Disease {heart_disease["Cholesterol"].max()}")
+print(f"Heart Disease {no_heart_disease["Cholesterol"].min()}")
+print(f"Heart Disease {heart_disease["Cholesterol"].max()}")
+#Heart Rate in progress
 Heart_Rate = hearts["MaxHR"]
-Sredni_Heart_Rate = Heart_Rate.mean()
-print(f"Średni Heart Rate wynosi {Sredni_Heart_Rate}")
+Average_Heart_Rate = Heart_Rate.mean()
+print(f"Average Heart Rate is {Average_Heart_Rate}")
